2dd406887d82da1d07004910db30e15134385226
Abdul Rehman <105493274+AbdulRehmanGHub@users.noreply.github.com>
2023-07-01 09:58:38 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apikey=7a0e2e32";
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=7a0e2e32";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
